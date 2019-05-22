5dc52ea4534a99db7b8ac6f249abe7dd0a8e4633
Leptos <leptos.0.null@gmail.com>
2019-05-22 16:27:20 -0700

    [Decryption] Add support to dump all loaded images, add support for FAT images

--- inject_decrypt/inject_decrypt.c
+++ inject_decrypt/inject_decrypt.c
@@ -6,185 +6,208 @@
 //
 //  compile:
 //    $(xcrun --sdk iphoneos --find clang) -isysroot $(xcrun --sdk iphoneos --show-sdk-path) -arch armv7 -arch arm64 -Os -dynamiclib
+//  most likely, the library will need to be signed in order to be injected (sign with `ldid -S` or similar)
 //
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgen.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
 #include <copyfile.h>
+#include <stdbool.h>
+#include <sys/stat.h>
 #include <mach-o/fat.h>
 #include <mach-o/dyld.h>
 #include <mach-o/loader.h>
 
-/* from Libc/Libc-1272.200.26/sys/crt_externs.c */
-#if !(__DYNAMIC__)
-#   warning ProgramVars may not be defined for this target
-#endif
-struct ProgramVars {
-    struct mach_header *mh;
-    int *NXArgcPtr;
-    const char ***NXArgvPtr;
-    const char ***environPtr;
-    const char **__prognamePtr;
-};
-/* end Libc/Libc-1272.200.26/sys/crt_externs.c */
-
 static const char __used
 _whatMsg[] = "@(#) inject_decrypt: decrypt Mach-O executables using injection",
-_whatUsg[] = "@(#) Usage: DYLD_INSERT_LIBRARIES=inject_decrypt.dylib <executable> <out_path>";
+_whatUsg[] = "@(#) Usage: DYLD_INSERT_LIBRARIES=inject_decrypt.dylib <executable> [-a] <out_path>";
 
 __attribute__((constructor, noreturn))
-static void dump(int argc, const char *argv[], const char *envp[], const char *apple[], const struct ProgramVars *vars) {
-    struct mach_header *mh = vars->mh;
-    if (mh == NULL) {
-        /*
-         * should this be used instead?
-         *
-         * mh = _dyld_get_image_header(0);
-         */
-        puts("Could not find mach header of loaded image");
-        exit(EXIT_FAILURE);
-    }
+static void dump(int argc, char *argv[]) {
+    const char *__restrict usageMessageFormat =
+    "Usage: DYLD_INSERT_LIBRARIES=inject_decrypt.dylib %s [-a] <out_path>\n"
+    "  -a    all images (out_path should be a non-existant directory)";
+    char usageMessage[strlen(usageMessageFormat) - 2 + strlen(argv[0]) + 1]; /* %s is -2, null term is +1 */
+    snprintf(usageMessage, sizeof(usageMessage), usageMessageFormat, argv[0]);
     
-    struct load_command *lc = NULL;
-    if (mh->magic == MH_MAGIC_64) {
-        lc = (void *)mh + sizeof(struct mach_header_64);
-    } else if (mh->magic == MH_MAGIC) {
-        lc = (void *)mh + sizeof(struct mach_header);
-    } else {
-        printf("Unknown magic: %#x\n", mh->magic);
+    if (argc < 2) {
+        puts(usageMessage);
         exit(EXIT_FAILURE);
     }
+    const char *outPath = argv[--argc];
     
-    const char *readPath = argv[0], *outPath = argv[1];
-    if (readPath == NULL) {
-        /*
-         * should this be used instead?
-         *
-         * while (*apple) {
-         *     const char *param = *apple++;
-         *     const char exec_path_key[] = "executable_path=";
-         *     const size_t exec_path_key_length = strlen(exec_path_key);
-         *     if (strncmp(param, exec_path_key, exec_path_key_length) == 0) {
-         *         readPath = param + exec_path_key_length;
-         *     }
-         * }
-         *
-         * or
-         *
-         * readPath = _dyld_get_image_name(0);
-         */
-        puts("Could not find executable path");
-        exit(EXIT_FAILURE);
-    }
-    if (outPath == NULL) {
-        puts("Output path required");
-        exit(EXIT_FAILURE);
-    }
+    bool wantsFrameworks = false;
     
-    int read_fd = open(readPath, O_RDONLY);
-    if (read_fd < 0) {
-        perror(readPath);
-        exit(EXIT_FAILURE);
-    }
-    
-    int write_fd = open(outPath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (write_fd < 0) {
-        perror(outPath);
-        exit(EXIT_FAILURE);
-    }
-    
-    struct fat_header fh;
-    if (read(read_fd, &fh, sizeof(fh)) != sizeof(fh)) {
-        perror("read fat_header");
-        exit(EXIT_FAILURE);
-    }
-    off_t write_offset = 0;
-    if (OSSwapBigToHostInt32(fh.magic) == FAT_MAGIC) {
-        struct fat_arch fa;
-        __typeof(*mh) mhl;
-        for (__typeof(fh.nfat_arch) i = 0; i < OSSwapBigToHostInt32(fh.nfat_arch); i++) {
-            if (read(read_fd, &fa, sizeof(fa)) != sizeof(fa)) {
-                perror("read fat_arch");
-                exit(EXIT_FAILURE);
-            }
-            off_t mh_offset = OSSwapBigToHostInt32(fa.offset);
-            if (pread(read_fd, &mhl, sizeof(mhl), mh_offset) != sizeof(mhl)) {
-                perror("read mach_header");
-                exit(EXIT_FAILURE);
-            }
-            
-            if (memcmp(&mhl, mh, sizeof(mhl)) == 0) {
-                write_offset = mh_offset;
+    int optc;
+    while ((optc = getopt(argc, argv, ":a")) >= 0) {
+        switch (optc) {
+            case 'a':
+                wantsFrameworks = true;
                 break;
-            }
+                
+            default:
+                puts(usageMessage);
+                exit(EXIT_FAILURE);
+                break;
         }
-        if (write_offset == 0) {
-            puts("Header magic is FAT_MAGIC, but could not find loaded slice in disk image");
+    }
+    
+    if (wantsFrameworks) {
+        if (mkdir(outPath, 0755) != 0) {
+            perror(outPath);
             exit(EXIT_FAILURE);
         }
-    } else if (OSSwapBigToHostInt32(fh.magic) == FAT_MAGIC_64) {
-        struct fat_arch_64 fa;
-        __typeof(*mh) mhl;
-        for (__typeof(fh.nfat_arch) i = 0; i < OSSwapBigToHostInt32(fh.nfat_arch); i++) {
-            if (read(read_fd, &fa, sizeof(fa)) != sizeof(fa)) {
-                perror("read fat_arch_64");
-                exit(EXIT_FAILURE);
-            }
-            off_t mh_offset = OSSwapBigToHostInt64(fa.offset);
-            if (pread(read_fd, &mhl, sizeof(mhl), mh_offset) != sizeof(mhl)) {
-                perror("read mach_header");
-                exit(EXIT_FAILURE);
-            }
-            
-            if (memcmp(&mhl, mh, sizeof(mhl)) == 0) {
-                write_offset = mh_offset;
-                break;
-            }
-        }
-        if (write_offset == 0) {
-            puts("Header magic is FAT_MAGIC_64, but could not find loaded slice in disk image");
-            exit(EXIT_FAILURE);
-        }
-    } else if (fh.magic == MH_MAGIC) {
-        
-    } else if (fh.magic == MH_MAGIC_64) {
-        
-    } else {
-        printf("Unknown magic: %x\n", fh.magic);
-        exit(EXIT_FAILURE);
-    }
-    if (lseek(read_fd, 0, SEEK_SET) != 0) {
-        perror("lseek start of file");
-        exit(EXIT_FAILURE);
-    }
-    if (fcopyfile(read_fd, write_fd, NULL, COPYFILE_DATA) < 0) {
-        perror("fcopyfile");
-        exit(EXIT_FAILURE);
     }
     
-    for (uint32_t i = 0; i < mh->ncmds; i++) {
-        // encryption_info_command and encryption_info_command_64 are the same except for some bottom padding
-        // similar to how mach_header and mach_header_64 are the same
-        if (lc->cmd == LC_ENCRYPTION_INFO || lc->cmd == LC_ENCRYPTION_INFO_64) {
-            struct encryption_info_command *eic = (__typeof(eic))lc;
+    for (uint32_t i = 0; i < _dyld_image_count(); i++) {
+        const struct mach_header *mh = _dyld_get_image_header(i);
+        
+        const struct load_command *lc = NULL;
+        if (mh->magic == MH_MAGIC_64) {
+            lc = (void *)mh + sizeof(struct mach_header_64);
+        } else if (mh->magic == MH_MAGIC) {
+            lc = (void *)mh + sizeof(struct mach_header);
+        } else {
+            printf("Unknown magic: %#x\n", mh->magic);
+            exit(EXIT_FAILURE);
+        }
+        
+        const char *readPath = _dyld_get_image_name(i);
+        struct stat statInfo;
+        if (stat(readPath, &statInfo) != 0) {
+            if (errno == ENOENT) {
+                errno = 0;
+                continue;
+            } else {
+                perror(readPath);
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        int read_fd = open(readPath, O_RDONLY);
+        if (read_fd < 0) {
+            perror(readPath);
+            exit(EXIT_FAILURE);
+        }
+        
+        const char *imageBase = basename((char *)readPath);
+        char writePath[strlen(outPath) + 1 + strlen(imageBase) + 1];
+        strcpy(writePath, outPath);
+        
+        if (wantsFrameworks) {
+            writePath[strlen(outPath)] = '/';
+            strcpy(writePath + strlen(outPath) + 1, imageBase);
+        } else {
+            writePath[strlen(outPath)] = 0;
+        }
+        
+        int write_fd = open(writePath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (write_fd < 0) {
+            perror(outPath);
+            exit(EXIT_FAILURE);
+        }
+        /**** start of FAT file support ****/
+        struct fat_header fh;
+        if (read(read_fd, &fh, sizeof(fh)) != sizeof(fh)) {
+            perror("read fat_header");
+            exit(EXIT_FAILURE);
+        }
+        uint64_t write_offset = 0;
+        if (OSSwapBigToHostInt32(fh.magic) == FAT_MAGIC) {
+            struct fat_arch fa;
+            struct mach_header mhl;
+            for (__typeof(fh.nfat_arch) arch = 0; arch < OSSwapBigToHostInt32(fh.nfat_arch); arch++) {
+                if (read(read_fd, &fa, sizeof(fa)) != sizeof(fa)) {
+                    perror("read fat_arch");
+                    exit(EXIT_FAILURE);
+                }
+                uint32_t mh_offset = OSSwapBigToHostInt32(fa.offset);
+                if (pread(read_fd, &mhl, sizeof(mhl), mh_offset) != sizeof(mhl)) {
+                    perror("read mach_header");
+                    exit(EXIT_FAILURE);
+                }
+                if (memcmp(&mhl, mh, sizeof(mhl)) == 0) {
+                    write_offset = mh_offset;
+                    break;
+                }
+            }
+            if (write_offset == 0) {
+                puts("Header magic is FAT_MAGIC, but could not find loaded slice in disk image");
+                exit(EXIT_FAILURE);
+            }
+        } else if (OSSwapBigToHostInt32(fh.magic) == FAT_MAGIC_64) {
+            struct fat_arch_64 fa;
+            struct mach_header mhl;
+            for (__typeof(fh.nfat_arch) arch = 0; arch < OSSwapBigToHostInt32(fh.nfat_arch); arch++) {
+                if (read(read_fd, &fa, sizeof(fa)) != sizeof(fa)) {
+                    perror("read fat_arch_64");
+                    exit(EXIT_FAILURE);
+                }
+                uint64_t mh_offset = OSSwapBigToHostInt64(fa.offset);
+                if (pread(read_fd, &mhl, sizeof(mhl), mh_offset) != sizeof(mhl)) {
+                    perror("read mach_header");
+                    exit(EXIT_FAILURE);
+                }
+                if (memcmp(&mhl, mh, sizeof(mhl)) == 0) {
+                    write_offset = mh_offset;
+                    break;
+                }
+            }
+            if (write_offset == 0) {
+                puts("Header magic is FAT_MAGIC_64, but could not find loaded slice in disk image");
+                exit(EXIT_FAILURE);
+            }
+        } else if (fh.magic == MH_MAGIC) {
             
-            if (eic->cryptid != 0) {
-                if (pwrite(write_fd, (void *)mh + eic->cryptoff, eic->cryptsize, eic->cryptoff + write_offset) != eic->cryptsize) {
-                    perror("pwrite crypt section");
+        } else if (fh.magic == MH_MAGIC_64) {
+            
+        } else {
+            printf("Unknown magic: 0x%x\n", fh.magic);
+            exit(EXIT_FAILURE);
+        }
+        
+        if (lseek(read_fd, 0, SEEK_SET) != 0) {
+            perror("lseek start of file");
+            exit(EXIT_FAILURE);
+        }
+        /**** end of FAT file support ****/
+        
+        if (fcopyfile(read_fd, write_fd, NULL, COPYFILE_DATA) < 0) {
+            perror("fcopyfile");
+            exit(EXIT_FAILURE);
+        }
+        
+        for (uint32_t cmd = 0; cmd < mh->ncmds; cmd++) {
+            // encryption_info_command and encryption_info_command_64 are the same except for some bottom padding
+            // similar to how mach_header and mach_header_64 are the same
+            if (lc->cmd == LC_ENCRYPTION_INFO || lc->cmd == LC_ENCRYPTION_INFO_64) {
+                const struct encryption_info_command *eic = (__typeof(eic))lc;
+                
+                if (eic->cryptid != 0) {
+                    if (pwrite(write_fd, (void *)mh + eic->cryptoff, eic->cryptsize, eic->cryptoff + write_offset) != eic->cryptsize) {
+                        perror("pwrite crypt section");
+                    }
+                    
+                    __typeof(eic->cryptid) zero = 0;
+                    if (pwrite(write_fd, &zero, sizeof(zero), (void *)&eic->cryptid - (void *)mh + write_offset) != sizeof(zero)) {
+                        perror("pwrite clearing encryption_info_command->cryptid");
+                    }
                 }
                 
-                __typeof(eic->cryptid) const zero = 0;
-                if (pwrite(write_fd, &zero, sizeof(zero), (void *)&eic->cryptid - (void *)mh + write_offset) != sizeof(zero)) {
-                    perror("pwrite clearing encryption_info_command->cryptid");
-                }
             }
-            
+            lc = (void *)lc + lc->cmdsize;
         }
-        lc = (void *)lc + lc->cmdsize;
+        close(read_fd);
+        close(write_fd);
+        
+        if (!wantsFrameworks) {
+            break;
+        }
     }
-    
     exit(EXIT_SUCCESS);
 }
